74bc8995d25732e942feeb07b07d21c45f9b4086
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-28 14:20:57 -0800

    pass fifth Item test

--- Parcels.Tests/ModelTests/ItemTests.cs
+++ Parcels.Tests/ModelTests/ItemTests.cs
@@ -19,7 +19,7 @@ namespace Parcels.Tests
     public void GetInstanceOfDimension_Item_Int()
     {
       Item newItem = new Item();
-      int[] dimension = {10, 15, 20};
+      int[] dimension = { 10, 15, 20 };
       newItem.Dimension = dimension;
       int[] result = newItem.Dimension;
       CollectionAssert.AreEqual(dimension, result);
@@ -29,17 +29,29 @@ namespace Parcels.Tests
     public void SetDimension_Dimension_Int()
     {
       Item newItem = new Item();
-      int[] dimension = {10, 15, 20};
-      int[] updatedDimension = {8, 10, 12};
+      int[] dimension = { 10, 15, 20 };
+      int[] updatedDimension = { 8, 10, 12 };
       newItem.Dimension = updatedDimension;
       int[] result = newItem.Dimension;
       CollectionAssert.AreEqual(updatedDimension, result);
     }
-  [TestMethod]
+    [TestMethod]
     public void GetAll_ReturnsEmptyList_ItemList()
     {
-      List<Item> newList = new List<Item> { };
-      List<Item> result = Item.GetAll();
+      List<int[]> newList = new List<int[]> { };
+      List<int[]> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      int[] dimension1 = { 10, 15, 20 };
+      int[] dimension2 = { 8, 10, 12 };
+      Item newItem1 = new Item(dimension1);
+      Item newItem2 = new Item(dimension2);
+      List<int[]> newList = new List<int[]> { dimension1, dimension2 };
+      List<int[]> result = Item.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
   }
