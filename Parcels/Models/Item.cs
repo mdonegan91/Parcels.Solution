494179c727bb0daeacb7ee0e68d10290df0b3aaa
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-28 14:28:02 -0800

    WIP

--- Parcels/Models/Item.cs
+++ Parcels/Models/Item.cs
@@ -5,7 +5,24 @@ namespace Parcels.Models
 {
   public class Item
   {
-    public int Dimension {get; set;}
+    public int[] Dimension { get; set; }
+
+    private static List<int[]> _instances = new List<int[]> { };
+
+    public Item()
+    {
+
+    }
+    public Item(int[] dimension)
+    {
+      Dimension = dimension;
+      _instances.Add(dimension);
+    }
+
+    public static List<int[]> GetAll()
+    {
+      return _instances;
+    }
   }
 }
 
